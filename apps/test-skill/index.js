25f62b88b5a28a1b0c95fa58195a73a92c21fb92
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2017-04-30 19:30:30 +0200

    modified skill

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -33,4 +33,42 @@ app.intent('reactProphet',
   }
 );
 
+app.intent("piep", 
+ 	{ 
+	"utterances":[ 
+		"piep ein mal"
+		]
+  },
+	function(request, response) {
+  // `getAsync` returns a Promise in this example. When
+  // returning a Promise, the response is sent after it
+  // resolves. If rejected, it is treated as an error.
+	return new Promise(function(resolve, reject) {
+  // do a thing, possibly async, then…
+
+	wait(5000);
+
+  if (true) {
+    resolve();
+  }
+  else {
+    reject(Error("It broke"));
+  }
+  
+  }).then(function(d){
+    response.say('piep');
+
+	});
+});
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
 module.exports = app;
+
+
