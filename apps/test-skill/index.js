09b80e74dc433c441e598db0765feb0af4a853e6
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2017-05-03 20:07:57 +0200

    changed alexa test skill to andre cécile laetita

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -2,73 +2,68 @@ module.change_code = 1;
 'use strict';
 
 
-var alexa = require( 'alexa-app' );
-var app = new alexa.app( 'test-skill' );
+var alexa = require('alexa-app');
+var app = new alexa.app('test-skill');
 
 
-app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
-} );
-
-
-
-
-app.error = function( exception, request, response ) {
-	console.log(exception)
-	console.log(request);
-	console.log(response);	
-	response.say( 'Sorry an error occured ' + error.message);
-};
-
-app.intent('reactProphet',
-  {
-    "slots":{"number":"NUMBER"}
-	,"utterances":[ 
-		"wer ist der react prophet"
-		]
-  },
-  function(request,response) {
-    //var number = request.slot('number');
-    response.say("Ganz klar. Natürlich bist du der React Prophet, Dennis");
-  }
-);
-
-app.intent("piep", 
- 	{ 
-	"utterances":[ 
-		"piep ein mal"
-		]
-  },
-	function(request, response) {
-  // `getAsync` returns a Promise in this example. When
-  // returning a Promise, the response is sent after it
-  // resolves. If rejected, it is treated as an error.
-	return new Promise(function(resolve, reject) {
-  // do a thing, possibly async, then…
-
-	wait(5000);
-
-  if (true) {
-    resolve();
-  }
-  else {
-    reject(Error("It broke"));
-  }
-  
-  }).then(function(d){
-    response.say('piep');
-
-	});
+app.launch(function(request, response) {
+    response.say('Welcome to your test skill').reprompt('Way to go. You got it to run. Bad ass.').shouldEndSession(false);
 });
 
-function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
+
+
+
+app.error = function(exception, request, response) {
+    console.log(exception)
+    console.log(request);
+    console.log(response);
+    response.say('Sorry an error occured ' + error.message);
+};
+
+app.intent('besuchen', {
+        "slots": { "number": "NUMBER" },
+        "utterances": [
+            "wie es der katie geht"
+        ]
+    },
+    function(request, response) {
+        //var number = request.slot('number');
+        response.say("Ganz klar. Natürlich bist du der React Prophet, Dennis");
+    }
+);
+
+app.intent("andre", {
+        "utterances": [
+            "wie es cécile laetita geht"
+        ]
+    },
+    function(request, response) {
+        // `getAsync` returns a Promise in this example. When
+        // returning a Promise, the response is sent after it
+        // resolves. If rejected, it is treated as an error.
+        return new Promise(function(resolve, reject) {
+            // do a thing, possibly async, then…
+
+            //wait(5000);
+
+            if (true) {
+                resolve();
+            } else {
+                reject(Error("It broke"));
+            }
+
+        }).then(function(d) {
+            response.say('Guten abend lieber andré vielleicht hasst du schon von mir gehört ich bin alexa! ich und dennis gratulieren euch ganz herzlich zum nachwuchs und wünschen euch vieren vie gesundheit auf erden. Ich hoffe wir lernen uns mal ganz persönlich kennen.');
+
+        });
+    });
+
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
 }
 
 module.exports = app;
-
-
